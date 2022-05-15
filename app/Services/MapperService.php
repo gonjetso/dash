701c342ada931c3e0fb033927ea0d85813a5efe3
unknown <gonjetsoc@gmail.com>
2022-05-15 20:45:11 +0200

    initial commit

---
+++ app/Services/MapperService.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Services;
+
+/**
+ * @deprecated
+ */
+class MapperService {
+    protected $collection;
+    protected $label;
+
+    public function __construct( $collection )
+    {
+        $this->collection   =   $collection;
+    }
+
+    public function retreive( $label )
+    {
+        $this->label    =   $label;
+        return $this;
+    }    
+
+    public function orReturn( callable $callback )
+    {
+        if ( ! isset( $this->collection[ $this->label ] ) ) {
+            $this->collection[ $this->label ]   =   $callback();
+        } 
+        return $this->collection[ $this->label ];
+    }
+}
